97f0d225a21b3e4fd642097a0ff951d77530ced3
DerAndr <AndrDerev@gmail.com>
2026-04-14 03:09:50 +0200

    Fix Lecture 01 and 02 practical notebook issues

--- lectures/lecture_02_data_preparation_part_1/practical_session/data_preparation_part1_practical_student_90min.py
+++ lectures/lecture_02_data_preparation_part_1/practical_session/data_preparation_part1_practical_student_90min.py
@@ -36,6 +36,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from IPython.display import display
 
 from sklearn.compose import ColumnTransformer
 from sklearn.datasets import fetch_openml
